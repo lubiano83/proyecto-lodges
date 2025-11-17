09ebd763a7db1ae26820c4381f3d3df95b47843a
lubiano83 <josepablolubiano@lubiano83-2.local>
2025-11-17 15:54:37 -0300

    ajustes varios

--- app/(pages)/admin/page.tsx
+++ app/(pages)/admin/page.tsx
@@ -1,10 +1,17 @@
 "use client";
-import Admin from "@/app/components/admin/Admin";
+import Module from "@/app/components/admin/Module";
+import Title from "@/app/components/Title";
 
 export default function AdminPage() {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center p-8">
-      <Admin />
+    <div className="w-full h-full flex flex-col justify-start items-center p-8 gap-6">
+      <Title>Panel de Administración:</Title>
+      <div className="flex justify-center items-center gap-6 flex-wrap">
+        <Module path={"/admin/home"}>Home</Module>
+        <Module path={"/admin/users"}>Users</Module>
+        <Module path={"/admin/lodges"}>Lodges</Module>
+        <Module path={"/admin/orders"}>Orders</Module>
+      </div>
     </div>
   );
 }
